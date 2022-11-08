6e66fcf8453e37810b1b1d1cb0ae244e15deb668
nguyenthienphu1206 <79633694+nguyenthienphu1206@users.noreply.github.com>
2022-11-08 21:53:05 +0700

    GET

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const server = express()
 
+// Tạo port =, nếu port bị trùng sẽ tìm port khác thay thế
 const port = 3000 || process.env.port
 
 const courses = [
@@ -9,14 +10,17 @@ const courses = [
     { id: 3, name: 'CSS' }
 ]
 
+// Khi mở trang web mặc định
 server.get('/', (req, res) => {
     res.send('Chào mừng bạn tham gia khóa học')
 })
 
+// Khi thêm /api/courses sẽ trả về danh sách khóa học
 server.get('/api/courses', (req, res) => {
     res.send(courses)
 })
 
+// Trả về khóa học theo id mình nhập, nếu không có sẽ thông báo
 server.get('/api/courses/:id', (req, res) => {
     const course = courses.find(courses => courses.id === parseInt(req.params.id))
     if (!course) {
